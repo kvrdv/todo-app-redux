9a38f678b27e3f403318144a54daf6ad68382a34
Ben Cubin <bencubin@gmail.com>
2023-10-03 14:23:12 +0600

    update filter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -7,12 +7,14 @@ const Counter: FC = () => {
 	let todos = useAppSelector((state) => state.todos);
 	const activeWeekday = useAppSelector((state) => state.filters.activeDay);
 
+	todos = todos.filter(({ weekday }) => weekday === activeWeekday);
+
 	const completedCount = todos.filter((element) => element.completed).length;
 	const activeCount = todos.length - completedCount;
 
 	return (
 		<Typography variant="h3" sx={styles.typography}>
-			{activeCount} to do / {completedCount} done / on {activeWeekday}
+			{activeCount} to do / {completedCount} done on {activeWeekday}
 		</Typography>
 	);
 };
--- src/components/counter/counter.styles.ts
+++ src/components/counter/counter.styles.ts
@@ -1,6 +1,6 @@
 const styles = {
 	typography: {
-		fontSize: '1.1rem',
+		fontSize: '1.3rem',
 		fontWeight: '600',
 		alignSelf: 'flex-end',
 		pb: '0.5rem',
